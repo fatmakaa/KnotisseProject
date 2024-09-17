28993d8fa4427ff50545a5cf87671a54e5f547f8
sumeyyeozbk <suhasu1337@gmail.com>
2024-09-17 23:09:04 +0300

    v2

--- src/test/java/stepDefinitions/US06_Sümeyye_StepDefs.java
+++ src/test/java/stepDefinitions/US06_Sümeyye_StepDefs.java
@@ -2,12 +2,14 @@ package stepDefinitions;
 
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import pages.Upholstery;
 import utilities.ConfigReader;
 import utilities.Driver;
 
 
 public class US06_Sümeyye_StepDefs {
-
+Upholstery upholstery=new Upholstery();
     @When("İlgili web siteye gidilir")
     public void i̇lgili_web_siteye_gidilir() throws InterruptedException {
 
@@ -15,7 +17,11 @@ public class US06_Sümeyye_StepDefs {
     }
 
 
+    @Then("UPHOLSTERY kategori adinin gorundugunu dogrulanir.")
+    public void upholsteryKategoriAdininGorundugunuDogrulanir() {
 
+     upholstery.upholsteryMenu.getText();
+        Assert.assertEquals("UPHOLSTERY",upholstery.upholsteryMenu.getText());
 
-
+    }
 }
---
+++ src/test/resources/features/US06_Smyye.feature
@@ -0,0 +1,5 @@
+Feature: US06
+  @US06_TC01
+  Scenario: US06_TC01
+    When İlgili web siteye gidilir.
+    Then UPHOLSTERY kategori adinin gorundugunu dogrulanir.
